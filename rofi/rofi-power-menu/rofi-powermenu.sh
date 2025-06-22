d124bcabf53163248723cbb2934c060ee66af649
NhomNhom0 <phingoc.nhomnhom@gmail.com>
2025-06-22 14:26:46 +0700

    rofi config

---
+++ rofi/rofi-power-menu/rofi-powermenu.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Power menu script using Rofi
+
+CHOSEN=$(printf "  Lock\n󰤄  Suspend\n  Reboot\n  Power Off\n󰗽  Log Out" | rofi -dmenu -i -p "Power Menu")
+
+case "$CHOSEN" in
+    "  Lock") light-locker-command --lock ;;  # Replace with your lock script if needed
+    "󰤄  Suspend") systemctl suspend-then-hibernate ;;
+    "  Reboot") systemctl reboot ;;
+    "  Power Off") systemctl poweroff ;;
+	"󰗽  Log Out") pkill -KILL -u "$USER" ;;
+    *) exit 1 ;;
+esac
